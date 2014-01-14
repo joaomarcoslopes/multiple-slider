bf32360638da1262eebee991b152c37b75e1538b
João Lopes <joao@Joao-Lopess-MacBook-Pro.local>
2014-01-14 15:31:12 -0200

    Primeira versão

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+/* jshint undef: false, unused: false */
+/* global console */
+
 $(document).on('ready', function(){
-	
+	var $containers = $('.triple-item-container');
+
+	$.each($containers, function(i, container){
+		var $current = $(container).find('.triple-item.active'),
+			$first = $(container).find('.triple-item').first();
+
+		setTimeout(function(){
+			setInterval(function() {
+				$current.removeClass('active');
+				$current = $current.next();
+				if (!$current.length) {
+					$current = $first;
+				}
+				$current.addClass('active');
+				var hasNext = $current.next().length !== 0 ? true : false;
+
+				if(!hasNext) {
+					$first.css('left', '312px');
+				}
+				else{
+					$first.removeAttr('style');
+				}
+			}, 3000);
+		}, 150*i);
+	});
+
 });
